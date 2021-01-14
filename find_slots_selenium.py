f57e279c3de6cc89aa385ee8ebe7dad06b6c595f
Artygo8 <arthur.gossuin@gmail.com>
2021-01-14 20:24:04 +0100

    connection via the web page

--- find_slots_selenium.py
+++ find_slots_selenium.py
@@ -6,20 +6,21 @@ class Intra42:
     def __init__(self):
         self.driver = webdriver.Firefox()
         self.my_url = "https://projects.intra.42.fr/projects/cpp-module-07/slots?team_id=3424654"
+        self.driver.get (self.my_url)
 
     def __del__(self):
         self.driver.close()
 
     def login(self):
-        self.driver.get (self.my_url)
         self.driver.find_element_by_id("user_login").send_keys(input("login: "))
         self.driver.find_element_by_id("user_password").send_keys(getpass.getpass("password: "))
         self.driver.find_element_by_name("commit").click()
-        if self.driver.current_url != self.my_url:
-            exit("Invalid login-password combination")
 
     def get_url(self):
         return self.driver.current_url
+
+    def is_connected(self):
+        return self.get_url() == self.my_url
 
     def is_slot_present(self):
         self.driver.refresh()
@@ -30,7 +31,10 @@ class Intra42:
 if __name__ == "__main__":
 
     intra = Intra42()
-    intra.login()
+
+    while not intra.is_connected():
+        print("You have 60 seconds to connect!")
+        time.sleep(60)
 
     while 1:
         try:
